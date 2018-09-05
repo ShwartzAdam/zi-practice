03f37bd264c9d1f5909c64a4b2c7c0740f192795
Adam Shwartz <adam@shwartz.co>
2018-09-05 18:35:02 +0300

    pr commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RowElmntComponent } from './row-elmnt/row-elmnt.component';
+import { ListElmntComponent } from './list-elmnt/list-elmnt.component';
+import {HttpClientModule} from "@angular/common/http";
+import {DataService} from "./data.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RowElmntComponent,
+    ListElmntComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/class/row.ts
@@ -0,0 +1,10 @@
+export class Row {
+  img?: string;
+  name: string;
+  title: string;
+  compLogo?: string;
+  compName: string;
+  compIndustry?: Array<string>;
+  compEmp: string;
+  lastUpdate: string;
+}
